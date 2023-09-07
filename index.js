68d1c53d3e2883fefb3cf22a60c78d70d9956e64
Laya Pullela <lpullela@ucsb.edu>
2023-09-07 07:32:19 -0700

    new scripts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 // Serve static files from the React app build directory
 app.use(express.static(path.join(__dirname, 'client/build')));

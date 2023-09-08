6b63e374ffcd671bdaa726d860207c5b41f3d2b1
Laya Pullela <lpullela@ucsb.edu>
2023-09-07 17:03:27 -0700

    cleaned up

--- scripts/symptoms.py
+++ scripts/symptoms.py
@@ -15,21 +15,7 @@ def basiccompare(userText):
     if len(results_list) == 0: 
         return "No Symptoms Detected"
     return ", ".join(results_list)
-
-def query(payload, API_URL, headers):
-    data = json.dumps(payload)
-    response = requests.request("POST", API_URL, headers=headers, data=data)
-    return json.loads(response.content.decode("utf-8"))
-
-def get_sym(inputtext):
-
-    headers = {"Authorization": f"Bearer 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"}
-    API_URL = "https://api-inference.huggingface.co/models/pmaitra/en_biobert_ner_symptom"
-
-    data = query({"inputs": inputtext, "options": {"wait_for_model": True}}, API_URL, headers)
-
-    return ", ".join([item['word'] for item in data])
-
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process user text.')
     parser.add_argument('--function', type=str, help='The name of the function to run.')
